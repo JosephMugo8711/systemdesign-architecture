996f662bcf35f3590aa2f3192d9757888c3d642f
JosephMugo8711 <wamiti8711@gmail.com>
2024-11-27 10:45:04 +0300

    Topics: Basics, Load balancing

---
+++ Fundamentals/basics/basics.js
@@ -0,0 +1,47 @@
+// What is system design
+//  Process of defining architecture, components and interfaces of a system
+
+// Goals 
+// Scalability
+// Reliability
+// Maintainability
+
+
+// Horizontal and vertical Scaling
+// Horizontal scaling - Adding more machines ( cpu, memory)
+// Vertical scaling - Upgrading machine resources ( cpu, RAM )
+
+// Horizontal scaling is better for large sysytems
+
+// What is capacity Estimation?
+// Predicting resources ( CPU, memory)) needed to handle user traffic
+
+// Terminology
+// 1. QPS ( Queries per second))
+// 2. Load testing
+// 3. Peak traffic
+
+
+// What is HTTP?
+// - Hypertext Transfer Protocol
+// - Foundation web communication
+// METHODS
+// GET, POST, PUT, DELETE
+// COMPONENTS
+// Response, Request, Status code
+
+
+// What is internet TCP/IP stack?
+// - Layers
+// 1. Application HTTP, DNS
+// 2. Transport TCP, UDP
+// 3. Network IP, routing
+// 4. Link Ethernet,  wi-fi
+
+
+// Example
+// What happens when you enter Google.com?
+// 1. DNS lookup: Resolves domain to IP
+// 2. TCP handshake: Connection established
+// 3. HTTP Request: browser sends a GET request
+// 4. Rendering HTML,CSS, JS Processed
---
+++ Fundamentals/basics/loadBalancing.js
@@ -0,0 +1,27 @@
+// What is load balancing?
+// Distributes incoming traffic across multiple servers
+
+// Benefits
+// 1. Fault tolerance
+// 2. Increased throughput
+// 3. Better user experience
+
+// Algorithms
+
+// 1. Round Robin
+// 2. Least connections
+// 3. IP hash
+
+
+// What is consistent hashing?
+// 1. Distributes data across multiple nodes with mininal redestribution when a node is added / removed
+// 2. Used in distributed systems ( eg. CDN, databases)
+
+
+// What is sharding?
+// Dividing data into partitions (shards)
+// Types
+// 1. Range-based : Divide by ranges (e.g ) A - M, N - Z)
+// 2. hash-based: Use hash functions
+
+
